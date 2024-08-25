0ec164e096ebc2706a05afa79284a9277a887b21
Creature-of-Habit <charliecharleston1121@gmail.com>
2024-08-25 23:59:32 +0100

    modifications

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+'''function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit/'''
 import requests
 
+
 def number_of_subscribers(subreddit):
+'''Returns number of subscribers for a subreddit if subreddit is valid. Returns 0 for invalid subreddit'''
 	response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
 	if response.status_code == 200:
 		return response.json()['data']['subscribers']
