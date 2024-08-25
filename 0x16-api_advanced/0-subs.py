5748fde170ed90af3896841f80523072c3f51ef5
Creature-of-Habit <charliecharleston1121@gmail.com>
2024-08-25 23:52:55 +0100

    corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,6 @@ import requests
 
 def number_of_subscribers(subreddit):
 	response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
-	print(response.status_code)
 	if response.status_code == 200:
 		return response.json()['data']['subscribers']
 	else:
