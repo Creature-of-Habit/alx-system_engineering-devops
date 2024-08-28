d1aae52f9615d78cec4dc31f024d6d7120fbd71d
Creature-of-Habit <charliecharleston1121@gmail.com>
2024-08-28 17:22:58 +0100

    updates

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,10 @@
 # Sets up client SSH configuration file to connect without password
 
-exec { 'modify_ssh_config_file':
-  command => 'echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> etc/ssh/ssh_config',
+exec { 'modify_id_file':
+  command => 'echo "IdentityFile ~/.ssh/school" >> etc/ssh/ssh_config',
   path    => '/bin/:/usr/bin',
-  return  => [0,1],
+}
+exec { 'modify_pssw_auth':
+  command => 'echo "PasswordAuthentication no" >> etc/ssh/ssh_config',
+  path    => '/bin/:/usr/bin',
 }
