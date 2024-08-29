b1b5c3a051018ac350c6f5554271fbe6898d5925
Creature-of-Habit <charliecharleston1121@gmail.com>
2024-08-29 11:54:10 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    
+
     Args:
         subreddit (str): The name of the subreddit.
 
@@ -18,20 +18,19 @@ def number_of_subscribers(subreddit):
     """
     # Set a custom User-Agent to avoid being blocked
     headers = {'User-Agent': 'MyRedditApp/0.1'}
-    
+
     # Construct the URL for the subreddit's about page
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
+
     try:
         # Make the request to the Reddit API
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         # Check if the request was successful and if the subreddit exists
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
-        else:
-            return 0
+        return 0
     except requests.RequestException:
         # Return 0 if there's any error making the request
         return 0
