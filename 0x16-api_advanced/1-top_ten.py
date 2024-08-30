f840f50b2e8f83e94593f8f1027005f688cd476d
Creature-of-Habit <charliecharleston1121@gmail.com>
2024-08-30 12:15:30 +0100

    top ten update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,25 @@
+
 #!/usr/bin/python3
-''' Function that queries the Reddit API.
-Prints the titles of the first 10 hot posts listed for a given subreddit'''
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    '''
-    Queries Reddit API and returns top ten hot topics.
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get("https://www.reddit.com/r/{}/hot.json".format(subreddit),
+                       headers={"User-Agent": "Custom"}, params={"limit": 10},)
 
-    Args:
-        Subreddit(str): subreddit
-
-    Returns:
-        List: Top ten hot topics if subreddit is valid, otherwise 0
-
-    '''
-    url = f'http://www.reddit.com/r/{subreddit}/hot.json'
-    # header = {"User-Agent": "Hot_ten"}
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-
-        hot_titles = []
-        for post in posts[:10]:
-            title = post.get('data', {}).get('title', 'No title')
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
             print(title)
-            hot_titles.append(title)
-        return hot_titles
     else:
         print(None)
-        return 0  # Return 0 if the subreddit is invalid
