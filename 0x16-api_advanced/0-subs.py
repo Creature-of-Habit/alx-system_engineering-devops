b5ea863ef1bd549f6af189b86d4326f71c0041a0
Creature-of-Habit <charliecharleston1121@gmail.com>
2024-09-11 17:23:34 +0100

    corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
+'''
 Function that queries the Reddit API.
 Returns the number of subscribers for a given subreddit.
-"""
+'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
+    '''
     Queries the Reddit API.
     Returns the number of subscribers for a given subreddit.
 
@@ -16,11 +16,12 @@ def number_of_subscribers(subreddit):
 
     Returns:
         int: The number of subscribers if the subreddit is valid, otherwise 0.
-    """
+    '''
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     header = {'User-Agent': '0-subs.py'}
 
-    response = requests.get(url=url, headers=header, allow_redirects=False)
+    response = requests.get(url=url, headers=header,
+                            allow_redirects=False, timeout=10)
     if response.status_code == 200:
         res = response.json()
         subs = res.get('data').get('subscribers')
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,14 @@
 
 #!/usr/bin/python3
-"""
+'''
 Function that queries the Reddit API.
 Prints the titles of the first 10 hot posts listed for a given subreddit.
-"""
+'''
 import requests
 
 
 def top_ten(subreddit):
-    """
+    '''
     Function that queries the Reddit API
     Prints the titles of the first 10 hot posts listed for a given subreddit
 
@@ -17,12 +17,12 @@ def top_ten(subreddit):
 
     Returns:
         void
-    """
+    '''
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     header = {"User-Agent": "1-top_ten"}
-    response = requests.get(url=url, headers=header, params={
-                            "limit": 10}, allow_redirects=False)
-
+    response = requests.get(url=url, headers=header,
+                            params={"limit": 10},
+                            allow_redirects=False, timeout=10)
     if response.status_code == 200:
         r_data = response.json().get("data").get("children")
         for res in r_data:
