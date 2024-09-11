1d5352885ea1a3370870aab9e2fd173d3c149136
Creature-of-Habit <charliecharleston1121@gmail.com>
2024-09-11 17:15:49 +0100

    corrections

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,6 +5,7 @@ Returns a list containing the titles of all hot articles for a given subreddit
 '''
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=''):
     '''
     Queries the Reddit API recursively.
@@ -21,7 +22,8 @@ def recurse(subreddit, hot_list=[], after=''):
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     header = {'User-Agent': '1-top_ten'}
     response = requests.get(url=url, headers=header,
-                            params={'after': after}, allow_redirects=False, timeout=10)
+                            params={'after': after},
+                            allow_redirects=False, timeout=10)
     if response.status_code != 200:
         return None
     else:
