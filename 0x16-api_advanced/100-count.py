50f4182a80371131aa0400a06588d42b19ab8e74
Creature-of-Habit <charliecharleston1121@gmail.com>
2024-09-11 17:06:12 +0100

    corrections

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-Recursive function that queries the Reddit API,Parses the title of all the hot articles
+Recursive function that queries the Reddit API.
+Parses the title of all the hot articles
 Prints a sorted count of given keywords
 Keywords are case-insensitive and delimited by spaces
 '''
@@ -8,7 +9,7 @@ Keywords are case-insensitive and delimited by spaces
 import requests
 
 
-def count_words(subreddit, word_list, after=""):
+def count_words(subreddit, word_list, after="", results={}):
     '''
     Function that queries the reddit API.
     Parses the a list of given keywords(titles of hot articles)
@@ -24,13 +25,11 @@ def count_words(subreddit, word_list, after=""):
     '''
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     header = {'User-Agent': '1-top_ten'}
+    param = {'after': after}
     response = requests.get(url=url, headers=header,
-                            params={'after': after}, allow_redirects=False, timeout=10)
+                            params=param, allow_redirects=False, timeout=10)
 
-    if response.status_code != 200:
-        return None
-
-    else:
+    if response.status_code == 200:
         resp = response.json().get('data')
         r_data = resp.get('children')
         after = resp.get('after')
@@ -39,9 +38,9 @@ def count_words(subreddit, word_list, after=""):
             data = res.get('data')
             title = data.get('title')
             print(title)
-            hot_list.append(title)
+
 
         if after is not None:
             return recurse(subreddit=subreddit, hot_list=hot_list, after=after)
         else:
-            return hot_list
+            return results
